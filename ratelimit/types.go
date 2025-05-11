fd3a3fc96e52082467e07f8787bdf4499b39192e
Kirby980 <hyz17779903860@gmail.com>
2025-05-11 17:00:48 +0800

    使用 go generate 命令

--- ratelimit/types.go
+++ ratelimit/types.go
@@ -4,6 +4,7 @@ import (
 	"context"
 )
 
+//go:generate mockgen -source=./types.go -package=limitermocks -destination=./mocks/limiter.mock.go Limiter
 type Limiter interface {
 	// Limit 限流
 	Limit(ctx context.Context, key string) (bool, error)
