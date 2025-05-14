aadc6b5cab35f3319cbf52d4564b11e9aad52075
Kirby980 <hyz17779903860@gmail.com>
2025-05-14 14:18:15 +0800

    添加QQ短信发送机制

--- generics.go
+++ generics.go
@@ -7,3 +7,11 @@ func ToOtherStruct[T any, U any](src []T, m func(idx int, src T) U) []U {
 	}
 	return dst
 }
+
+func SliceToPtrSlice[T any](src []T) []*T {
+	dst := make([]*T, len(src))
+	for i, s := range src {
+		dst[i] = &s
+	}
+	return dst
+}
