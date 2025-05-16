65b5147bc6b9b3467c5d99aae1c1ef84bbc12f84
Kirby980 <hyz17779903860@gmail.com>
2025-05-16 15:57:19 +0800

    添加批量消费者代码

---
+++ saramax/batch_consumer.go
@@ -0,0 +1,81 @@
+package saramax
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/IBM/sarama"
+	"github.com/Kirby980/study/webook/pkg/logger"
+)
+
+type BatchConsumer[T any] struct {
+	l             logger.LoggerV1
+	fn            func(msg []*sarama.ConsumerMessage, t []T) error
+	batchSize     int
+	batchDuration time.Duration
+}
+
+func NewBathConsumer[T any](l logger.LoggerV1, fn func(msg []*sarama.ConsumerMessage, t []T) error) *BatchConsumer[T] {
+	return &BatchConsumer[T]{
+		l:             l,
+		fn:            fn,
+		batchSize:     10,
+		batchDuration: time.Second,
+	}
+}
+
+func (h BatchConsumer[T]) Setup(session sarama.ConsumerGroupSession) error {
+	h.l.Info("setup")
+	return nil
+}
+
+func (h BatchConsumer[T]) Cleanup(session sarama.ConsumerGroupSession) error {
+	h.l.Info("cleanup")
+	return nil
+}
+func (h BatchConsumer[T]) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	//批量消费
+	msgsCh := claim.Messages()
+	for {
+		ctx, cancel := context.WithTimeout(context.Background(), h.batchDuration)
+		var last *sarama.ConsumerMessage
+		done := false
+		msgs := make([]*sarama.ConsumerMessage, 0, h.batchSize)
+		ts := make([]T, 0, h.batchSize)
+		for i := 0; i < h.batchSize && !done; i++ {
+			select {
+			case msg, ok := <-msgsCh:
+				if !ok {
+					cancel()
+					// 代表消费者被关闭了
+					return nil
+				}
+				last = msg
+				var t T
+				err := json.Unmarshal(msg.Value, &t)
+				if err != nil {
+					h.l.Error("反序列化失败", logger.Error(err), logger.String("topic", msg.Topic),
+						logger.Int64("offset", msg.Offset), logger.Int32("partition", msg.Partition))
+					continue
+				}
+				msgs = append(msgs, msg)
+				ts = append(ts, t)
+			case <-ctx.Done():
+				done = true
+			}
+		}
+		cancel()
+		if len(msgs) == 0 {
+			continue
+		}
+		err := h.fn(msgs, ts)
+		if err != nil {
+			h.l.Error("调用批量接口失败")
+		}
+		if last != nil {
+			session.MarkMessage(last, "")
+		}
+
+	}
+}
