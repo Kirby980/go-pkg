4fff52777ff81b08119ff83f99362af6c6990670
Kirby980 <hyz17779903860@gmail.com>
2025-05-27 16:30:22 +0800

    添加排行榜服务以generate mock代码

--- generics.go
+++ generics.go
@@ -1,5 +1,6 @@
 package pkg
 
+// ToOtherStruct 函数将一个类型为 T 的切片转换为另一个类型为 U 的切片
 func ToOtherStruct[T any, U any](src []T, m func(idx int, src T) U) []U {
 	dst := make([]U, len(src))
 	for i, s := range src {
@@ -8,6 +9,7 @@ func ToOtherStruct[T any, U any](src []T, m func(idx int, src T) U) []U {
 	return dst
 }
 
+// 将切片转换为指针切片
 func SliceToPtrSlice[T any](src []T) []*T {
 	dst := make([]*T, len(src))
 	for i, s := range src {
