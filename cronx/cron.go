025e2c0bf433340671997962f3071e37c1eca319
Kirby980 <hyz17779903860@gmail.com>
2025-05-29 12:53:48 +0800

    refactor: 重构cron包，迁移至cronx并定义Job接口

--- cronx/cron.go
+++ cronx/cron.go
@@ -1,4 +1,4 @@
-package pkg
+package cronx
 
 import (
 	"github.com/robfig/cron/v3"
---
+++ cronx/job_builder.go
@@ -0,0 +1,56 @@
+package cronx
+
+import (
+	"context"
+	"strconv"
+	"time"
+
+	"github.com/Kirby980/study/webook/pkg/logger"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/robfig/cron/v3"
+	"go.opentelemetry.io/otel/trace"
+)
+
+type CronJobBuilder struct {
+	l      logger.LoggerV1
+	p      *prometheus.SummaryVec
+	tracer trace.Tracer
+}
+
+func NewCronJobBuilder(l logger.LoggerV1, p prometheus.SummaryOpts, labelNames []string, tracer trace.Tracer) *CronJobBuilder {
+	ps := prometheus.NewSummaryVec(p, labelNames)
+	prometheus.MustRegister(ps)
+	return &CronJobBuilder{l: l, p: ps, tracer: tracer}
+}
+func (b *CronJobBuilder) Build(job Job) cron.Job {
+	name := job.Name()
+	return cronJobFuncAdapter(func() error {
+		ctx, span := b.tracer.Start(context.Background(), name)
+		defer span.End()
+		start := time.Now()
+		b.l.Info("任务开始",
+			logger.String("job", name))
+		var success bool
+		defer func() {
+			b.l.Info("任务结束",
+				logger.String("job", name))
+			duration := time.Since(start).Milliseconds()
+			b.p.WithLabelValues(name,
+				strconv.FormatBool(success)).Observe(float64(duration))
+		}()
+		err := job.Run(ctx)
+		success = err == nil
+		if err != nil {
+			span.RecordError(err)
+			b.l.Error("运行任务失败", logger.Error(err),
+				logger.String("job", name))
+		}
+		return nil
+	})
+}
+
+type cronJobFuncAdapter func() error
+
+func (c cronJobFuncAdapter) Run() {
+	_ = c()
+}
