d57937829d476adecbe68a18e962b9b7928d968a
Kirby980 <hyz17779903860@gmail.com>
2025-06-16 17:47:40 +0800

    feat: 添加不停机迁移策略

--- generics.go
+++ generics.go
@@ -17,3 +17,26 @@ func SliceToPtrSlice[T any](src []T) []*T {
 	}
 	return dst
 }
+
+// SliceDiffSet 计算两个切片的差集 只支持 comparable 类型
+func SliceDiffSet[T comparable](src []T, dst []T) []T {
+	srcMap := toMap(src)
+	for _, val := range dst {
+		delete(srcMap, val)
+	}
+	var ret = make([]T, 0, len(srcMap))
+	for key := range srcMap {
+		ret = append(ret, key)
+	}
+
+	return ret
+}
+
+// toMap 将切片转换为 map
+func toMap[T comparable](src []T) map[T]struct{} {
+	var dataMap = make(map[T]struct{}, len(src))
+	for _, v := range src {
+		dataMap[v] = struct{}{}
+	}
+	return dataMap
+}
