2f6b9a179ecca5d25de0ae912a69e625b0f7bdfd
Kirby980 <hyz17779903860@gmail.com>
2025-06-23 17:36:58 +0800

    ✨ feat: 实现grpc基于etcd的服务注册与发现

--- grpcx/server.go
+++ grpcx/server.go
@@ -1,20 +1,103 @@
 package grpcx
 
 import (
+	"context"
 	"net"
+	"strconv"
+	"time"
 
+	"github.com/Kirby980/study/webook/pkg/logger"
+	"github.com/Kirby980/study/webook/pkg/netx"
+	etcdv3 "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/client/v3/naming/endpoints"
 	"google.golang.org/grpc"
 )
 
 type Server struct {
 	*grpc.Server
-	Addr string
+	Port      int
+	EtcdAddrs []string
+	Name      string
+	key       string
+	em        endpoints.Manager
+	client    *etcdv3.Client
+	kaCancel  context.CancelFunc
+	L         logger.Logger
 }
 
 func (s *Server) Serve() error {
-	l, err := net.Listen("tcp", s.Addr)
+	l, err := net.Listen("tcp", ":"+strconv.Itoa(s.Port))
 	if err != nil {
 		panic(err)
 	}
+	err = s.register()
+	if err != nil {
+		return err
+	}
 	return s.Server.Serve(l)
+}
+
+func (s *Server) register() error {
+	etcd, err := etcdv3.New(etcdv3.Config{
+		Endpoints: s.EtcdAddrs,
+	})
+	if err != nil {
+		return err
+	}
+	s.client = etcd
+	em, err := endpoints.NewManager(etcd, "service/"+s.Name)
+	if err != nil {
+		return err
+	}
+	s.em = em
+	addr := netx.GetOutboundIP() + ":" + strconv.Itoa(s.Port)
+	key := "service/" + s.Name + "/" + addr
+	s.key = key
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	leaseResp, err := etcd.Grant(ctx, 30)
+	if err != nil {
+		return err
+	}
+	err = em.AddEndpoint(ctx, key, endpoints.Endpoint{
+		Addr: addr,
+	}, etcdv3.WithLease(leaseResp.ID))
+	if err != nil {
+		return err
+	}
+	kaCtx, kaCancel := context.WithCancel(context.Background())
+	ch, err := s.client.KeepAlive(kaCtx, leaseResp.ID)
+	s.kaCancel = kaCancel
+	if err != nil {
+		return err
+	}
+	go func() {
+		for kaResp := range ch {
+			// 正常就是打印一下 DEBUG 日志啥的
+			s.L.Debug(kaResp.String())
+		}
+	}()
+	return nil
+}
+
+func (s *Server) Close() error {
+	if s.kaCancel != nil {
+		s.kaCancel()
+	}
+	if s.em != nil {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		err := s.em.DeleteEndpoint(ctx, s.key)
+		if err != nil {
+			return err
+		}
+	}
+	if s.client != nil {
+		err := s.client.Close()
+		if err != nil {
+			return err
+		}
+	}
+	s.Server.GracefulStop()
+	return nil
 }
--- netx/ip.go
+++ netx/ip.go
@@ -1,10 +1,30 @@
 package netx
 
-import "net"
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
+	"time"
+)
 
-// GetOutboundIP 获得对外发送消息的 IP 地址
+// GetOutboundIP 获取本机外网IP
 func GetOutboundIP() string {
-	// DNS 的地址，国内可以用 114.114.114.114
+	// 先获取本机IP
+	ip := getLocalOutboundIP()
+	if ip == "" {
+		return ""
+	}
+
+	// 根据IP地理位置选择DNS服务器
+	dnsServer := selectDNSServerByLocation(ip)
+
+	// 使用选定的DNS服务器重新获取IP（确保准确性）
+	return getOutboundIPWithDNS(dnsServer)
+}
+
+// getLocalOutboundIP 获取本地外网IP（使用默认DNS）
+func getLocalOutboundIP() string {
 	conn, err := net.Dial("udp", "8.8.8.8:80")
 	if err != nil {
 		return ""
@@ -13,4 +33,114 @@ func GetOutboundIP() string {
 
 	localAddr := conn.LocalAddr().(*net.UDPAddr)
 	return localAddr.IP.String()
+}
+
+// getOutboundIPWithDNS 使用指定DNS服务器获取外网IP
+func getOutboundIPWithDNS(dnsServer string) string {
+	conn, err := net.Dial("udp", dnsServer+":80")
+	if err != nil {
+		return ""
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP.String()
+}
+
+// selectDNSServerByLocation 根据IP地理位置选择DNS服务器
+func selectDNSServerByLocation(ip string) string {
+	// 检查是否为内网IP
+	if isPrivateIP(ip) {
+		// 内网IP，使用国内DNS
+		return "114.114.114.114"
+	}
+
+	// 检查是否为国内IP段
+	if isChineseIP(ip) {
+		return "114.114.114.114"
+	}
+
+	// 国外IP，使用Google DNS
+	return "8.8.8.8"
+}
+
+// isPrivateIP 检查是否为内网IP
+func isPrivateIP(ip string) bool {
+	parsedIP := net.ParseIP(ip)
+	if parsedIP == nil {
+		return false
+	}
+
+	// 检查是否为内网IP段
+	privateRanges := []string{
+		"10.0.0.0/8",     // A类私有网络
+		"172.16.0.0/12",  // B类私有网络
+		"192.168.0.0/16", // C类私有网络
+		"127.0.0.0/8",    // 回环地址
+	}
+
+	for _, cidr := range privateRanges {
+		_, network, err := net.ParseCIDR(cidr)
+		if err != nil {
+			continue
+		}
+		if network.Contains(parsedIP) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// isChineseIP 检查是否为国内IP（简化版本）
+func isChineseIP(ip string) bool {
+	// 这里可以集成IP地理位置数据库
+	// 简化版本：通过在线API查询
+	return queryIPLocation(ip)
+}
+
+// queryIPLocation 查询IP地理位置
+func queryIPLocation(ip string) bool {
+	// 使用免费的IP地理位置API
+	url := fmt.Sprintf("http://ip-api.com/json/%s?fields=countryCode", ip)
+
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	resp, err := client.Get(url)
+	if err != nil {
+		// 如果查询失败，默认使用国内DNS
+		return true
+	}
+	defer resp.Body.Close()
+
+	var result struct {
+		CountryCode string `json:"countryCode"`
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return true
+	}
+
+	// 检查是否为中国
+	return result.CountryCode == "CN"
+}
+
+// GetOutboundIPWithFallback 带降级策略的IP获取
+func GetOutboundIPWithFallback() string {
+	// 尝试使用国内DNS
+	ip := getOutboundIPWithDNS("114.114.114.114")
+	if ip != "" {
+		return ip
+	}
+
+	// 如果失败，尝试Google DNS
+	ip = getOutboundIPWithDNS("8.8.8.8")
+	if ip != "" {
+		return ip
+	}
+
+	// 最后尝试默认方法
+	return getLocalOutboundIP()
 }
